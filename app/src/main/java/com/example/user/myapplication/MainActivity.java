2f2c2f2dc5fe8869cab805b24c5de0d6e7b9ed66
User <real0224@gmail.com>
2016-10-31 22:25:09 +0800

    2nd attempt

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.user.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void getMessage(View view){
+        Intent intentGetMessage = new Intent(this,Main2Activity.class);
+        startActivityForResult(intentGetMessage,2);
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == 2){
+
+            if(data!= null){
+                String message = data.getStringExtra("MESSAGE");
+
+                TextView textViewMessage = (TextView)findViewById(R.id.textViewMessage);
+                textViewMessage.setText("Message is :"+message);
+
+            }
+        }
     }
 }
